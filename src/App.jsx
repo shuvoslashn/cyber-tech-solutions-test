f776e92a573163c9cf0a956a1cf0d112ed0993d9
Shuvo Sarker <sarker3664@gmail.com>
2023-09-20 19:39:39 +0600

    Initial Setup Done

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div className='w-full h-screen flex flex-col gap-y-5 justify-center items-center pb-24'>
+            <h3 className='text-3xl'>environment setup done</h3>
+            <h1 className='text-8xl font-[500]'>Ready to work</h1>
+        </div>
+    );
+}
+
+export default App;
